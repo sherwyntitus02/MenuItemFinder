97f513b79aac05a5c9df6842f45faa482241f693
Sherwyn Titus <sherwyntitus02@gmail.com>
2025-04-06 00:27:53 -0700

    Initial commit

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ItemFinderWeb.Models;
+using System.Collections.ObjectModel;
+
+namespace ItemFinderWeb.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult Index(string search)
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Assets", "ItemData.json");
+            var jsonData = System.IO.File.ReadAllText(filePath);
+            var menuItems = JsonConvert.DeserializeObject<List<ItemModel>>(jsonData);
+            if (!string.IsNullOrWhiteSpace(search) && menuItems != null)
+            {
+                menuItems = menuItems.Where(item =>item.ItemName.Contains(search, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+            else
+            {
+                //menuItems = menuItems.ToList();
+            }
+
+            ViewBag.Search = search;
+            return View(menuItems);
+        }
+    }
+}
---
+++ Models/ItemModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ItemFinderWeb.Models
+{
+    public class ItemModel
+    {
+        public string ItemId { get; set; }
+        public int ItemCode { get; set; }
+        public string ItemName { get; set; }
+        public float ItemPrice { get; set; }
+        public string PriceDisplay
+        {
+            get
+            {
+                return $"₹ {ItemPrice}.00";
+            }
+        }
+    }
+}
